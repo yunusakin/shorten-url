2241826324780b0239dc2aac05cc61dd2d5ee17b
yunusakin <yunusakin@gmail.com>
2023-07-02 22:02:13 +0300

    Shorten Url Base Structure - major edits

---
+++ src/main/java/com/yunusakin/shortenurl/ShortenurlApplication.java
@@ -0,0 +1,16 @@
+package com.yunusakin.shortenurl;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Short URL API", version = "1.0", description = "API for generating short URLs and redirecting to original URLs"))
+public class ShortenurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenurlApplication.class, args);
+	}
+
+}
